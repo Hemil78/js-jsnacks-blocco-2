b36aacd79bc55d97686cb29bca1a3394d8bf4d3e
Hemil78 <zanattahemil@gmail.com>
2021-08-27 16:02:22 +0200

    correzione snack-3

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,19 +1,19 @@
 //Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi,
 // Gatsby vuole generare una falsa lista di 3 invitati.
 
-//creo una lista di nomi
-var cognomi = ["Rossi", "Bianchi", "Verdi"];
-//creo una lista di cognomi
-var nomi = ["Mario", "Carlo","Lucio"];
-//genero una lista di nomi e cognomi casuale
-var cognomiRandom = cognomi[Math.floor(Math.random() * cognomi.length)];
-console.log(cognomiRandom);
-var nomiRandom = nomi[Math.floor(Math.random() * nomi.length)];
-console.log(nomiRandom);
-//associo nomi e cognomi random e li stampo
-var randomName = cognomiRandom + nomiRandom;
-
-while (randomName < 4) {
-    console.log(randomName);
-    randomName++;
+//creo una lista di nomi e cognomi
+var nomi = ["Mario", "Alessio", "Luca"];
+var cognomi = ["Rossi", "Bianchi", "Catullo"];
+//creo lista vuota
+var invitati = [];
+//ciclo per generare nomi e cognomi random
+for (var i = 1; i <= 3; i++) {
+    var randomNome = Math.floor(Math.random() * nomi.length);
+    var randomCognome = Math.floor(Math.random() * cognomi.length);
+    //aggiungo nome lista
+    var nomeCompleto = nomi[randomNome] + " " + cognomi[randomCognome];
+    invitati.push(nomeCompleto);
 }
+
+//stampo la lista
+console.log(invitati);
